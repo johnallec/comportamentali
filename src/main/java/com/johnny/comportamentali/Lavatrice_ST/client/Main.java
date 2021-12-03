e7127f900ec77513c1388b4a10d1d99aec1b524d
johnallec <clegnn95p30c352m@studenti.unical.it>
2021-12-03 10:25:24 +0100

    Added main in Lavatrice_ST/client

---
+++ src/main/java/com/johnny/comportamentali/Lavatrice_ST/client/Main.java
@@ -0,0 +1,21 @@
+package com.johnny.comportamentali.Lavatrice_ST.client;
+
+import com.johnny.comportamentali.Lavatrice_ST.state.Lavatrice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Lavatrice lavatrice = new Lavatrice();
+		lavatrice.avvioLavaggio();
+		lavatrice.chiudiPortellone();
+		lavatrice.scegliMetodoDiLavaggio();
+		lavatrice.inserisciDetersivo();
+		System.out.println("\nIn ordine logico:\n");
+		lavatrice.inserisciGettone();
+		lavatrice.inserisciDetersivo();
+		lavatrice.scegliMetodoDiLavaggio();
+		lavatrice.chiudiPortellone();
+		lavatrice.avvioLavaggio();
+	}
+
+}
